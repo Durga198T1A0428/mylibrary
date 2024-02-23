db4f287537d44de7254feafef86d2ca94d5eba13
Durga prasad chimmili <142197469+Durga198T1A0428@users.noreply.github.com>
2024-02-23 22:39:56 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,4 +8,6 @@ def build()
 }
 def deploy(jobname,ip,context)
 {
-  
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}  
+
